52869d344bd979d18a58121dbace20a04e6088e5
Najwa Zaid <najwa576949@gmail.com>
2026-05-18 11:16:15 +0800

    Update and rename Test Cases to Test Cases.m

---
+++ Test Cases.m
@@ -0,0 +1,4 @@
+
+<img width="1918" height="1009" alt="TC001_SuccessfulLogin" src="https://github.com/user-attachments/assets/d351d867-c69f-4def-adaa-72b408dc33fe" />
+<img width="1343" height="583" alt="TC003_InvalidUsernameLogin" src="https://github.com/user-attachments/assets/5f9fd7ce-fbb0-4838-af6d-19b23ce4ed59" />
+<img width="1349" height="630" alt="TC002_InvalidPasswordLogin" src="https://github.com/user-attachments/assets/9be2356a-0743-43ea-930f-16b414c6c03e" />
